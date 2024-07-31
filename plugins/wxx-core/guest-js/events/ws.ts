5a1e55cef5a97bc385fcd259967c5c90e1104fed
delongchen <fg456@vip.qq.com>
2024-08-01 07:34:23 +0800

    feat(plugin): add riot games api dto.

---
+++ plugins/wxx-core/guest-js/events/ws.ts
@@ -0,0 +1,5 @@
+import { listen } from "@tauri-apps/api/event";
+
+interface a {
+  name?: string,
+}
--- plugins/wxx-core/src/v2/consts.rs
+++ plugins/wxx-core/src/v2/consts.rs
@@ -1,3 +1,5 @@
+pub mod events;
+
 pub const RIOT_GAMES_PEM_BYTES: &[u8] = include_bytes!("./riotgames.pem");
 
 pub const LCU_WS_EVENT: &str = "LCU_WS_EVENT";
