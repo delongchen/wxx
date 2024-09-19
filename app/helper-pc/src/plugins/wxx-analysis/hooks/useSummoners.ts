d8ded2e2435ef255e541b8a2d1e7390b6cf47b01
delongchen <fg456@vip.qq.com>
2024-09-19 20:22:16 +0800

    add proto lib

--- app/helper-pc/src/plugins/wxx-analysis/hooks/useSummoners.ts
+++ app/helper-pc/src/plugins/wxx-analysis/hooks/useSummoners.ts
@@ -1,28 +1,61 @@
 import { useEffect, useState } from 'react';
 import { gameFlowPhaseStream } from '../lcu';
-import { GameflowPhaseEnum } from 'wxx-protobufs/lcu.gameflow';
+
+// This is just a comment, you can delete it after reading it
+//
+// wxx-protobufs is designed to share info for other user
+// and is best not to be used within other plugin
+// import { GameflowPhaseEnum } from 'wxx-protobufs/lcu.gameflow';
+//
+// You can import it using
+// import { GameflowPhase } from 'tauri-plugin-wxx-core'
+
 import { getGameflowSession } from 'tauri-plugin-wxx-core/lcu-api/gameflow';
 import { TeamPlayer } from 'tauri-plugin-wxx-core';
 
 export const useSummoners = () => {
   const [teammates, setTeammates] = useState<TeamPlayer[]>([]);
   const [enemies, setEnemies] = useState<TeamPlayer[]>([]);
+
+  /**
+   * This is just a comment, you can delete it after reading it
+   *
+   * When using subscribe, a subscription will be returned
+   * You may need to unsubscribe after `useEffect`
+   * Here is an example tool
+   *
+   * const useSubscribe = <T>(source: Observable<T>, ob: (data: T) => void) => {
+   *   useEffect(() => {
+   *     const subscription = source.subscribe(ob);
+   *
+   *     return () => {
+   *       subscription.unsubscribe();
+   *     };
+   *   }, []);
+   * };
+   *
+   * example:
+   *
+   * useSubscribe(gameFlowPhaseStream, phase => {
+   *   ...
+   * })
+   */
   useEffect(() => {
     gameFlowPhaseStream.subscribe(phase => {
-      switch (GameflowPhaseEnum[phase]) {
-        case GameflowPhaseEnum.ChampSelect:
+      switch (phase) {
+        case 'ChampSelect': // GameflowPhaseEnum.ChampSelect:
           // fetch teammates
           getGameflowSession().then(session => {
             setTeammates(session.gameData.teamOne);
           });
           break;
-        case GameflowPhaseEnum.InProgress:
+        case 'InProgress': // GameflowPhaseEnum.InProgress:
           // fetch enemies
           getGameflowSession().then(session => {
             setEnemies(session.gameData.teamTwo);
           });
           break;
-        case GameflowPhaseEnum.EndOfGame:
+        case 'EndOfGame': // GameflowPhaseEnum.EndOfGame:
           setTeammates([]);
           setEnemies([]);
           break;
