dc7f556dfc284ba18093a2be4c6e8465409ad8b0
delongchen <fg456@vip.qq.com>
2024-09-19 18:31:19 +0800

    marge from yuqi

--- plugins/wxx-core/guest-js/api/lcu-api/match-history.ts
+++ plugins/wxx-core/guest-js/api/lcu-api/match-history.ts
@@ -4,6 +4,10 @@ import { Game, MatchHistory } from '../../types/lcu-api/match-history';
 export const getCurrentSummonerMatchHistory = api('/lol-match-history/v1/products/lol/current-summoner/matches')
   .noPayload('get');
 
+export const getMatchHistoryV3 = api(
+  '/lol-match-history/v3/matchlist/account/:summonerId?startIndex&endIndex',
+).noPayload<MatchHistory>('get');
+
 export const getMatchHistory =
   api('/lol-match-history/v1/products/lol/:puuid/matches?begIndex&endIndex')
     .noPayload<MatchHistory>('get');
