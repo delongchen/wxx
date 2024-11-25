9fd58e10d25e56e2d76410cb1bbb80a3d0ecb2fb
delongchen <fg456@vip.qq.com>
2024-11-25 11:35:32 +0800

    style(proto): move lib to lib_js

--- libs/protobufs/gen-proto.bat
+++ libs/protobufs/gen-proto.bat
@@ -1,13 +1,13 @@
 set "BASE_DIR=%CD%"
 set "SORUCE_DIR=%BASE_DIR%\src"
 
-del %BASE_DIR%\lib\*
+del %BASE_DIR%\lib_js\*
 
 protoc.exe ^
     --proto_path=%SORUCE_DIR% ^
     --plugin=protoc-gen-ts_proto=".\\node_modules\\.bin\\protoc-gen-ts_proto.CMD" ^
     --ts_proto_opt=outputIndex=true ^
-    --ts_proto_out=".\\lib" ^
+    --ts_proto_out=".\\lib_js" ^
     %SORUCE_DIR%\lcu\*.proto ^
     %SORUCE_DIR%\common\*.proto ^
     %SORUCE_DIR%\rest\*.proto
